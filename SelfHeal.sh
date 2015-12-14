9b53c79b1f14dbc2be2904a7f87fa4dbfabd59e8
cyvan31 <lucas.vance@jamfsoftware.com>
2015-12-14 14:33:33 -0600

    Update SelfHeal.sh

--- SelfHeal.sh
+++ SelfHeal.sh
@@ -33,7 +33,7 @@
 #
 #       version 2.0 by Lucas Vance
 #	Added log rotation, log archiving and log clean up LCV 11-23-15
-#	Add changes to the variables for more consitent Self Heal checks LCV 11-23-15
+#	Modified variables for more consitent Self Heal checks LCV 11-23-15
 #
 #####################################################################################################
 
