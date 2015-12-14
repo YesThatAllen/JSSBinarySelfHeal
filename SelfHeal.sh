d0c3731f7184608f8c8c22259b9fcd851da8f9eb
cyvan31 <lucas.vance@jamfsoftware.com>
2015-12-14 14:33:00 -0600

    Update SelfHeal.sh

--- SelfHeal.sh
+++ SelfHeal.sh
@@ -2,7 +2,7 @@
 
 #####################################################################################################
 #
-# Copyright (c) 2014, JAMF Software, LLC.  All rights reserved.
+# Copyright (c) 2015, JAMF Software, LLC.  All rights reserved.
 #
 #
 #       THIS SOFTWARE IS PROVIDED BY JAMF SOFTWARE, LLC "AS IS" AND ANY
